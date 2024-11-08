fb1a658f9fe5a8df598cfd3deeec93ad9ffb1f3e
3DM1chu <vumigames@gmail.com>
2024-11-09 00:12:53 +0100

    Init2 - as WebAPI instead of MVC

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CryptoAlertsBackend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
