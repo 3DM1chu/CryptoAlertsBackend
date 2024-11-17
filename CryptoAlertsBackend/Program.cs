83ef7ad2e8c826bfed196cc1bbda698a1e1b0f1a
3DM1chu <vumigames@gmail.com>
2024-11-17 22:23:15 +0100

    Dockerization :)

--- CryptoAlertsBackend/Program.cs
+++ CryptoAlertsBackend/Program.cs
@@ -1,23 +1,21 @@
 using CryptoAlertsBackend.Models;
 using Microsoft.EntityFrameworkCore;
-using DotNetEnv;
-using Microsoft.AspNetCore.Builder;
-
-// Load .env file
-Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
+string SQL_SERVER_IP = Environment.GetEnvironmentVariable("DB_IP");
+string SQL_DATABASE_NAME = Environment.GetEnvironmentVariable("SQL_DATABASE_NAME");
+string DB_SA_PASSWD = Environment.GetEnvironmentVariable("DB_SA_PASSWD");
+string CONNECTION_STRING = $"Server={SQL_SERVER_IP};Database={SQL_DATABASE_NAME};User=sa;Password={DB_SA_PASSWD};TrustServerCertificate=True;MultipleActiveResultSets=True";
+
 // Add services to the container
 builder.Services.AddDbContext<EndpointContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection").Length == 0
-    ? 
-    builder.Configuration.GetConnectionString("DevConnection")
-    :
-    "Server=localhost,25557;Database=TokenManagement;User ID=sa;Password=Password_123;TrustServerCertificate=True;MultipleActiveResultSets=True")
+    options => options.UseSqlServer(CONNECTION_STRING)
+    //"Server=localhost,25557;Database=TokenManagement;User ID=sa;Password=Password_123;TrustServerCertificate=True;MultipleActiveResultSets=True")
 );
 
 builder.Services.AddControllers();
+/*
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -26,8 +24,8 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
-});
-//builder.Services.AddHostedService<TestBgService>();
+});*/
+
 builder.Services.AddScoped<AssetService>();
 
 var app = builder.Build();
@@ -40,9 +38,18 @@ if (app.Environment.IsDevelopment())
 
 // Middleware
 app.UseCors("AllowAll");
-app.UseHttpsRedirection(); // Enables HTTPS redirection if needed
 
 // Important: Register routes defined by controllers
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<EndpointContext>();
+    try
+    {
+        db.Database.Migrate();
+    }
+    catch (Exception) { }
+}
 
 app.Run();
